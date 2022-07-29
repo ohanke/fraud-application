a87cac82ec88cce1316d0115420f779409186fea
ohanke <steamoha@wp.pl>
2022-07-29 15:27:25 +0200

    Fixed typo in fraud clien uri.

--- clients/src/main/java/com/oscarhanke/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/oscarhanke/clients/fraud/FraudClient.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "fraud")
+@FeignClient("fraud")
 public interface FraudClient {
-    @GetMapping(path = "api/v1/fraud-check/{customerId}")
+    @GetMapping(path = "api/vi/fraud-check/{customerId}")
     FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId);
 }
--- fraud/src/main/java/com/oscarhanke/fraud/FraudApplication.java
+++ fraud/src/main/java/com/oscarhanke/fraud/FraudApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
+@EnableFeignClients(
+        basePackages = "com.oscarhanke.clients"
+)
 public class FraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudApplication.class, args);
