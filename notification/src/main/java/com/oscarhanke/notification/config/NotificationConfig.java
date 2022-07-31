f50adca899220d76833316244310c4380e3bc594
ohanke <steamoha@wp.pl>
2022-07-31 11:48:25 +0200

    Added notification queue configuration.

---
+++ notification/src/main/java/com/oscarhanke/notification/config/NotificationConfig.java
@@ -0,0 +1,19 @@
+package com.oscarhanke.notification.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class NotificationConfig {
+
+    @Value("${rabbitmq.exchanges.internal}")
+    private String internalExchange;
+
+    @Value("${rabbitmq.queues.notification}")
+    private String notification;
+
+    @Value("${rabbitmq.routing-keys.internal-notification}")
+    private String internalNotificationRoutingKey;
+}
